1edd368fe5ab00f6d9a5230040cd82f9efa597a1
Shabab Ali <shababali672@gmail.com>
2023-04-01 13:25:15 +0530

    commit

--- src/Dashboard/Login.js
+++ src/Dashboard/Login.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import Inputs from '../components/Inputs';
 import Button from '../components/Button';
+import { useNavigate } from 'react-router-dom';
 
-function handleLogin(){
-  // window.location.href = "https://unrivaled-seahorse-94d284.netlify.app/admin"
-  window.location.href = "/admin"
-  
-}
+// function handleLogin(){
+//   // window.location.href = "https://unrivaled-seahorse-94d284.netlify.app/admin"
+//   // window.location.href = "/admin"
+// Navigate('/admin')  
+// }
 
 export default function Login() {
+const navigate = useNavigate();
+function handleLogin(){
+  // window.location.href = "https://unrivaled-seahorse-94d284.netlify.app/admin"
+  // window.location.href = "/admin"
+  navigate('/admin')  
+}
   return (
     <div className="login-wrapper ">  
       <h3>Login</h3>

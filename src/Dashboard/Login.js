0671bb02fb9ee5993eebfea19e558aab3b735d8d
Shabab Ali <shababali672@gmail.com>
2023-04-17 21:37:22 +0530

    commit

--- src/Dashboard/Login.js
+++ src/Dashboard/Login.js
@@ -1,33 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Inputs from '../components/Inputs';
 import Button from '../components/Button';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-// function handleLogin(){
-//   // window.location.href = "https://unrivaled-seahorse-94d284.netlify.app/admin"
-//   // window.location.href = "/admin"
-// Navigate('/admin')  
-// }
+
 
 export default function Login() {
-const navigate = useNavigate();
-function handleLogin(){
-  // window.location.href = "https://unrivaled-seahorse-94d284.netlify.app/admin"
-  // window.location.href = "/admin"
-  navigate('/admin')  
-}
+  const [inputs, setInputs] = useState({email:"" , password:""})
+  function handleInputs(e) {
+    const { name, value } = e.target;
+    setInputs((prevInputs) => ({  
+      ...prevInputs,
+      [name]: value,
+    }));
+    console.log(inputs)
+  }
+  function handleLogin() {
+    // const navigate = useNavigate();
+    const url = "https://nigst.onrender.com/sadmin/login";
+    const data = {
+      email: `${inputs.email}`,
+      password: `${inputs.password}`
+    }
+    console.log(data)
+    axios.post(url, data).then((res) => {
+      console.log(res)
+    }).catch((error) => {
+      console.log(error)
+    })
+    // navigate('/admin') 
+  }
   return (
-    <div className="login-wrapper ">  
+    <div className="login-wrapper ">
       <h3>Login</h3>
-      <Inputs type={"email"} placeholder={"Enter email"}/>
-      <Inputs type={"password"} placeholder={"Enter Password"}/>
+      <Inputs type={"email"} placeholder={"Enter email"} name={"email"} fun={handleInputs} />
+      <Inputs type={"password"} placeholder={"Enter Password"} name={"password"} fun={handleInputs} />
       <select>
         <option>Select</option>
         <option>NIGST Admin</option>
         <option>Faculty Admin</option>
         <option>Faculty Member</option>
       </select>
-      <Button value={"Login"} fun={handleLogin}/>
+      <Button value={"Login"} fun={handleLogin} />
     </div>
   )
 }

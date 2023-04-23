6fe54e22f3b9fc611ecbe16a206cd793e9476124
Shabab Ali <shababali672@gmail.com>
2023-04-23 10:02:22 +0530

    commit

--- src/Dashboard/ResetPassword.js
+++ src/Dashboard/ResetPassword.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState } from 'react'
 
 export default function ResetPassword() {
@@ -12,13 +13,27 @@ export default function ResetPassword() {
       ...prevInput , [name]:value
     }))
   }
+
+  function handleResetPassword(){
+    const url = "https://nigst.onrender.com/sauth/change";
+    const data = {
+      facultyId:`${input.faculty}`,
+      oldPassword:`${input.password}`,
+      newPassword:`${input.CPassword}`
+    }
+    axios.patch(url,data).then((res)=>{
+      console.log(res)
+    }).catch((error)=>{
+      console.log(error);
+    })
+  }
   return (
     <div className='department-creation-wrapper'>
        <h3>Reset Password</h3>
-      <input type="password" placeholder='Faculty Id'  name="faculty" onChange={handleInputs}></input>
+      <input type="text" placeholder='Faculty Id'  name="faculty" onChange={handleInputs}></input>
       <input type="password" placeholder='Old Password'  name="password" onChange={handleInputs}></input>
       <input type="password" placeholder='New Password' name="CPassword" onChange={handleInputs}></input>
-      <button>Submit</button>
+      <button onClick={handleResetPassword}>Submit</button>
     </div>
   )
 }

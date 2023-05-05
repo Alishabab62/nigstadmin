af9ee834b5b3e78319ba91b17b8141473fa5e063
Abhishek Paul <105283319+PAULPHOENIX555@users.noreply.github.com>
2023-05-05 16:39:36 +0530

    css faculty creation

--- src/Dashboard/CreationFacultyMember.js
+++ src/Dashboard/CreationFacultyMember.js
@@ -9,82 +9,82 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 export default function CreationFacultyMember() {
-  const [faculty , setFaculty] = useState([]);
-  const [viewFrame , setViewFrame] = useState(false);
-  const [facultyView , setFacultyView] = useState([]);
+  const [faculty, setFaculty] = useState([]);
+  const [viewFrame, setViewFrame] = useState(false);
+  const [facultyView, setFacultyView] = useState([]);
   const [successAlert, setSuccessAlert] = useState(false);
   const [failAlert, setFailAlert] = useState(false);
   const [open, setOpen] = React.useState(false);
-  const [userEmail , setUserEmail] = useState("");
-  const [userStatus , setUserStatus] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userStatus, setUserStatus] = useState("");
 
   // const [emptyFieldAlert, setEmptyFieldAlert] = useState(false);
   const [input, setInput] = useState({
-    f_name:"",
-    l_name:"",
-    m_name:"",
-    email:"",
-    phone:"",
-    education:"",
-    designation:"",
+    f_name: "",
+    l_name: "",
+    m_name: "",
+    email: "",
+    phone: "",
+    education: "",
+    designation: "",
   });
-  const [gender , setGender] = useState("");
-  const [facultyInput , setFacultyInput] = useState("");
-  const [login , setLogin] = useState("");
-  const [user,setUser] = useState("");
-  const dobRef = useRef();
+  const [gender, setGender] = useState("");
+  const [facultyInput, setFacultyInput] = useState("");
+  const [login, setLogin] = useState("");
+  const [user, setUser] = useState("");
+  const dobRef = useRef(null);
 
-  function handleInputs(e){
-    const {name,value} = e.target;
-    setInput((prevInput)=>({
-      ...prevInput , [name]:value
+  function handleInputs(e) {
+    const { name, value } = e.target;
+    setInput((prevInput) => ({
+      ...prevInput, [name]: value
     }));
   }
 
-  useEffect(()=>{
-    const url = "http://nigstserver-env-4.eba-upjrs3n3.ap-south-1.elasticbeanstalk.com/admin/faculty_show";
-    axios.get(url).then((res)=>{
+  useEffect(() => {
+    const url = "https://nigst.onrender.com/admin/faculty_show";
+    axios.get(url).then((res) => {
       setFaculty(res.data)
-    }).catch((error)=>{
+    }).catch((error) => {
       console.log(error)
     })
-   facultyViewFun();
-   let user = JSON.parse(localStorage.getItem("user"));
-   setUser(user)
-  },[])
+    facultyViewFun();
+    let user = JSON.parse(localStorage.getItem("user"));
+    setUser(user)
+  }, [])
 
-  function facultyViewFun(){
-    const urlView = "http://nigstserver-env-4.eba-upjrs3n3.ap-south-1.elasticbeanstalk.com/sauth/faculty_view"
-    axios.get(urlView).then((res)=>{
+  function facultyViewFun() {
+    const urlView = "https://nigst.onrender.com/sauth/faculty_view"
+    axios.get(urlView).then((res) => {
       console.log(res)
       setFacultyView(res.data.data.reverse())
-    }).catch((error)=>{
+    }).catch((error) => {
       console.log(error);
     })
   }
 
-  function handleCreationMembers(){
-    const url = "http://nigstserver-env-4.eba-upjrs3n3.ap-south-1.elasticbeanstalk.com/sauth/create";
+  function handleCreationMembers() {
+    const url = "https://nigst.onrender.com/sauth/create";
     const data = {
-      first_name:`${input.f_name}`,
-      middle_name:`${input.m_name}`,
-      last_name:`${input.l_name}`,
-      dob:`${dobRef.current.value}`,
-      phone:`${input.phone}`,
-      email:`${input.email}`,
-      gender:`${gender}`,
-      education:`${input.education}`,
-      designation:`${input.designation}`,
-      loginAccess:`${login}`,
-      faculty:`${facultyInput}`,
+      first_name: `${input.f_name}`,
+      middle_name: `${input.m_name}`,
+      last_name: `${input.l_name}`,
+      dob: `${dobRef.current.value}`,
+      phone: `${input.phone}`,
+      email: `${input.email}`,
+      gender: `${gender}`,
+      education: `${input.education}`,
+      designation: `${input.designation}`,
+      loginAccess: `${login}`,
+      faculty: `${facultyInput}`,
     }
-    axios.post(url,data).then((res)=>{
+    axios.post(url, data).then((res) => {
       facultyViewFun()
       setSuccessAlert(true)
       setTimeout(() => {
         setSuccessAlert(false)
       }, 5000);
-    }).catch((error)=>{
+    }).catch((error) => {
       setFailAlert(true)
       setTimeout(() => {
         setFailAlert(false)
@@ -93,139 +93,189 @@ export default function CreationFacultyMember() {
     })
   }
 
-function viewData(){
-  setViewFrame(!viewFrame);
-}
-
-function setFalseLoginAccess(e){
-  const url = "http://nigstserver-env-4.eba-upjrs3n3.ap-south-1.elasticbeanstalk.com/admin/access";
-  const data = {
-      email:`${userEmail}`,
-      access:"false"
+  function viewData() {
+    setViewFrame(!viewFrame);
   }
-  axios.patch(url,data).then((res)=>{
-    facultyViewFun();
-  }).catch((error)=>{
-    console.log(error)
-  })
-}
 
-function setTrueLoginAccess(e){
-  const url = "http://nigstserver-env-4.eba-upjrs3n3.ap-south-1.elasticbeanstalk.com/admin/access";
-  const data = {
-      email:`${userEmail}`,
-      access:"true"
+  function setFalseLoginAccess(e) {
+    const url = "https://nigst.onrender.com/admin/access";
+    const data = {
+      email: `${userEmail}`,
+      access: "false"
+    }
+    axios.patch(url, data).then((res) => {
+      facultyViewFun();
+    }).catch((error) => {
+      console.log(error)
+    })
   }
-  axios.patch(url,data).then((res)=>{
-    facultyViewFun();
-  }).catch((error)=>{
-    console.log(error)
-  })
-}
 
-function statusChange(){
-  setOpen(false)
-  userStatus === "green" ? setFalseLoginAccess() : setTrueLoginAccess() 
-}
+  function setTrueLoginAccess(e) {
+    const url = "https://nigst.onrender.com/admin/access";
+    const data = {
+      email: `${userEmail}`,
+      access: "true"
+    }
+    axios.patch(url, data).then((res) => {
+      facultyViewFun();
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
+  function statusChange() {
+    setOpen(false)
+    userStatus === "green" ? setFalseLoginAccess() : setTrueLoginAccess()
+  }
   const handleClickOpen = (e) => {
     setOpen(true);
     setUserEmail(e.target.getAttribute("data"));
     setUserStatus(e.target.style.color);
   };
 
-function closeModal(){
-  setOpen(false)
-}
+
+
+  function closeModal() {
+    setOpen(false)
+  }
+
+  const [searchData, setSearchData] = useState("");
+
+  const handleInputChange1 = (event) => {
+    setSearchData(event.target.value);
+    const input = event.target.value.toLowerCase();
+    const rows = document.querySelectorAll("#faculties tr");
+
+    rows.forEach((row) => {
+      const cells = row.querySelectorAll("td");
+      let shouldHide = true;
+
+      cells.forEach((cell) => {
+        if (cell.textContent.toLowerCase().includes(input)) {
+          shouldHide = false;
+        }
+      });
+
+      if (shouldHide) {
+        row.classList.add("hidden");
+      } else {
+        row.classList.remove("hidden");
+      }
+    });
+  };
 
   return (
     <>
-    <div style={{position:"absolute" , top:"120px" , right:"20px"}}>
-      {
-        viewFrame ? <button onClick={viewData}>Create Faculty</button> : <button onClick={viewData}>View Created Faculty</button>
-      }
-    </div>
-  {
-    viewFrame ?   <div className='user-details-wrapper'>
-    <table>
-      <tbody>
-        <tr>
-            <th>S.No</th>
-            <th>Faculty Id.</th>
-            <th>Created At</th>
-            <th>First Name</th>
-            <th>Middle Name</th>
-            <th>Last Name</th>
-            <th>Phone</th>
-            <th>Email</th>
-            <th>Faculty</th>
-            <th>Designation</th>
-            <th>Gender</th>
-            <th>Edu.</th>
-            <th>Admin Verification</th>
-        </tr>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
         {
-          facultyView.map((data,index)=>{
-            return data.faculty===user.faculty ? 
-             (
-              <tr>
-                <td>{index+1}</td>
-                <td>{data.faculty_id}</td>
-                <td>{data.created_on_date_time}</td>
-                <td>{data.first_name}</td>
-                <td>{data.middle_name}</td>
-                <td>{data.last_name}</td>
-                <td>{data.phone}</td>
-                <td>{data.email}</td>
-                <td>{data.faculty}</td>
-                <td>{data.designation}</td>
-                <td>{data.gender}</td>
-                <td>{data.education}</td>
-                <td>{data.admin_verified ? <button data={data.email} onClick={handleClickOpen} style={{backgroundColor:"green" , color:"green" , borderRadius:"50%" , height:"40px" , width:"40px"}} ></button> : <button  data={data.email} onClick={handleClickOpen} style={{ height:"40px" , width:"40px" , backgroundColor:"red" , color:"red" , borderRadius:"50%"}}></button>}</td>
-              </tr>
-            ) : ""
-          })
+          viewFrame ? <button className='toggle_btn' onClick={viewData}>Create Faculty</button> : <button className='toggle_btn' onClick={viewData}>View Created Faculty</button>
         }
-          </tbody>
-    </table>
-    </div>  : ""
-  }
-    {
-      !viewFrame ? <div className="faculty-member-creation-wrapper">  
-      <h3>Creation Faculty Member</h3>
-      {successAlert ? <Alert severity="success" style={{marginBottom:"10px"}}>Faculty Created Successfully</Alert> : ""}
-        {failAlert ? <Alert severity="error" style={{marginBottom:"10px"}}>Something Went Wrong Please try again later</Alert> : ""}
-        {/* {emptyFieldAlert ? <Alert severity="error" style={{marginBottom:"10px"}}>All fields required</Alert> : ""} */}
-      <Inputs type={"text"} placeholder={"First Name"} name={"f_name"} fun={handleInputs}/>
-      <Inputs type={"text"} placeholder={"Middle Name"} name={"m_name"} fun={handleInputs}/>
-      <Inputs type={"text"} placeholder={"Last name"} name={"l_name"} fun={handleInputs}/>
-      <select onChange={(e)=>(setFacultyInput(e.target.value))}>
-        <option >Select Faculty</option>
-        {
-          faculty.map((data)=>{
-            return <option key={data.id} value={data.name}>{data.name}</option>
-          })
-        }
-      </select>
-      <input type='date' ref={dobRef}></input>
-      <select onChange={(e)=>(setGender(e.target.value))}>
-        <option>Select Gender</option>
-        <option value={"male"}>Male</option>
-        <option value={"female"}>Female</option>
-        <option value={"other"}>Other</option>
-      </select>
-      <Inputs type={"email"} placeholder={"Enter email"} name={"email"} fun={handleInputs}/>
-      <Inputs type={"tel"} placeholder={"Enter Phone"} name={"phone"} fun={handleInputs}/>
-      <input type='text' placeholder='Enter Highest Qualification' name={"education"} onChange={handleInputs}></input>
-      <input type='text' placeholder='Enter Designation' name={"designation"} onChange={handleInputs}></input>
-      {/* <Inputs type={"password"} placeholder={"Enter Password"} name={"password"} fun={handleInputs}/> */}
-      <div style={{display:"flex" , alignItems:"center"}}>
-      <input type="radio" value="true" name="admin verification" onChange={(e)=>(setLogin(e.target.value))}/> <label>Login Access</label>
-      <input type="radio" value="false" name="admin verification" onChange={(e)=>(setLogin(e.target.value))}/> <label>No Login Access</label>
       </div>
-      <button  onClick={handleCreationMembers}>Submit</button>
-    </div> : ""
-    }
-    <Dialog
+
+      {
+        viewFrame ?
+        <div>
+        <input type="text" id="SearchInput" placeholder="Search Faculties" value={searchData} onChange={handleInputChange1} />
+        
+        <div className='user-details-wrapper'>
+          
+          <table>
+            <thead>
+              <tr>
+                <th colSpan="13" style={{ textAlign: "center", backgroundColor: "#ffcb00" }}>FACULTIES</th>
+              </tr>
+              <tr>
+                <th>S.No</th>
+                <th>Faculty Id.</th>
+                <th>Created At</th>
+                <th>First Name</th>
+                <th>Middle Name</th>
+                <th>Last Name</th>
+                <th>Phone</th>
+                <th>Email</th>
+                <th>Faculty</th>
+                <th>Designation</th>
+                <th>Gender</th>
+                <th>Edu.</th>
+                <th>Admin Verification</th>
+              </tr>
+            </thead>
+            <tbody id='faculties'>
+              {
+                facultyView.map((data, index) => {
+                  return data.faculty === user.faculty ?
+                    (
+                      <tr>
+                        <td>{index + 1}</td>
+                        <td>{data.faculty_id}</td>
+                        <td>{data.created_on_date_time}</td>
+                        <td>{data.first_name}</td>
+                        <td>{data.middle_name}</td>
+                        <td>{data.last_name}</td>
+                        <td>{data.phone}</td>
+                        <td>{data.email}</td>
+                        <td>{data.faculty}</td>
+                        <td>{data.designation}</td>
+                        <td>{data.gender}</td>
+                        <td>{data.education}</td>
+                        <td>{data.admin_verified ? <button data={data.email} onClick={handleClickOpen} style={{ backgroundColor: "green", color: "green", borderRadius: "50%", height: "30px", width: "30px" }} ></button> : <button data={data.email} onClick={handleClickOpen} style={{ height: "40px", width: "40px", backgroundColor: "red", color: "red", borderRadius: "50%" }}></button>}</td>
+                      </tr>
+                    ) : ""
+                })
+              }
+            </tbody>
+          </table>
+        </div>
+        </div> : ""
+      }
+      {
+        !viewFrame ? <div className="faculty-member-creation-wrapper">
+          <h3 style={{margin:"20px auto"}}>Creation Faculty Member</h3>
+          {successAlert ? <Alert severity="success" style={{ marginBottom: "10px" }}>Faculty Created Successfully</Alert> : ""}
+          {failAlert ? <Alert severity="error" style={{ marginBottom: "10px" }}>Something Went Wrong Please try again later</Alert> : ""}
+          {/* {emptyFieldAlert ? <Alert severity="error" style={{marginBottom:"10px"}}>All fields required</Alert> : ""} */}
+          <div className="grid-container">
+            <input type="text" placeholder="First Name" name="f_name" onChange={handleInputs} />
+            <input type="text" placeholder="Middle Name" name="m_name" onChange={handleInputs} />
+            <input type="text" placeholder="Last Name" name="l_name" onChange={handleInputs} />
+          </div>
+          <div className="grid-container">
+            <input type='text' onFocus={() => { dobRef.current.type = 'date' }} placeholder="Date of Birth" ref={dobRef} />
+
+            <select onChange={(e) => (setGender(e.target.value))}>
+              <option>Select Gender</option>
+              <option value={"male"}>Male</option>
+              <option value={"female"}>Female</option>
+              <option value={"other"}>Other</option>
+            </select>
+            <select onChange={(e) => (setFacultyInput(e.target.value))}>
+              <option >Select Faculty</option>
+              {
+                faculty.map((data) => {
+                  return <option key={data.id} value={data.name}>{data.name}</option>
+                })
+              }
+            </select>
+          </div>
+          <div className="grid2-container">
+            <Inputs type={"email"} placeholder={"Enter email"} name={"email"} fun={handleInputs} />
+            <Inputs type={"tel"} placeholder={"Enter Phone"} name={"phone"} fun={handleInputs} />
+          </div>
+          <div className="grid2-container">
+            <input type='text' placeholder='Enter Highest Qualification' name={"education"} onChange={handleInputs}></input>
+            <input type='text' placeholder='Enter Designation' name={"designation"} onChange={handleInputs}></input>
+          </div><br />
+          {/* <Inputs type={"password"} placeholder={"Enter Password"} name={"password"} fun={handleInputs}/> */}
+
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <p style={{ marginRight: "1rem" }}>Login Access</p>
+            <input type="radio" value="true" name="admin verification" onChange={(e) => (setLogin(e.target.value))} /> <label style={{ marginRight: "1rem" }}>Yes</label>
+            <input type="radio" value="false" name="admin verification" onChange={(e) => (setLogin(e.target.value))} /> <label>No</label>
+          </div>
+          <button style={{ width: '70%', margin: '20px auto 0' }} onClick={handleCreationMembers}>Submit</button>
+        </div> : ""
+      }
+      <Dialog
         open={open}
         onClose={closeModal}
         aria-labelledby="alert-dialog-title"
@@ -238,11 +288,11 @@ function closeModal(){
         </DialogContent>
         <DialogActions>
           <Button onClick={closeModal}>Disagree</Button>
-          <Button onClick={statusChange}  autoFocus>
+          <Button onClick={statusChange} autoFocus>
             Agree
           </Button>
         </DialogActions>
-      </Dialog> 
+      </Dialog>
     </>
   )
 }

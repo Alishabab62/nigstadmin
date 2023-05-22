43fda49399bebe394662f2d0cbcf506d866f4b25
alishabab62 <shababali672@gmail.com>
2023-05-22 21:33:18 +0530

    course report

--- src/Dashboard/FacultyReportSubmission.js
+++ src/Dashboard/FacultyReportSubmission.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useRef, useState } from 'react'
+
 
 export default function FacultyReportSubmission() {
+const [courseTitle,setCourseTitle] = useState("");
+const [remark,setRemark] = useState("");
+const pdf = useRef();
+function handleReportSubmission(){
+  const url = "";
+  const data={};
+  axios.post(url,data).then((res)=>{
+    console.log(res)
+  }).catch((error)=>{
+    console.log(error)
+  })
+}
+
+
+
   return (
     <div className='department-creation-wrapper'>
        <h3>Course Scheduling</h3>
-       <select>
+       <select onChange={(e)=>setCourseTitle(e.target.value)}>
             <option>Select Course Title</option>
        </select>
-        <input placeholder='Enter Remark' type='text' name='remark'></input>
-        <input type='file' ></input>
-        <button>Submit</button>
+        <input placeholder='Enter Remark' type='text' name='remark' onChange={(e)=>setRemark(e.target.value)}></input>
+        <input type='file' ref={pdf}></input>
+        <button onClick={handleReportSubmission}>Submit</button>
     </div>
   )
 }

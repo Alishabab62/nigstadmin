2f5b5bd3dbd193b65e7b3daa7ff1e04f4bcb5fda
alishabab62 <shababali672@gmail.com>
2023-06-14 21:36:41 +0530

    commit

--- src/Dashboard/Faculty.js
+++ src/Dashboard/Faculty.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import Logo from '../images/NIGST LOGO Small.svg'
+import React, { useState,useEffect } from 'react'
+import Logo from '../images/logo1.png'
 import '../CSS/app.css';
 import ResetPassword from './ResetPassword';
 import FacultyReportSubmission from './FacultyReportSubmission';
@@ -21,11 +21,24 @@ export default function Faculty() {
     window.location.hash = '/';
     localStorage.clear("user")
  }
+ const [userName, setUserName] = useState('');
+
+
+
+
+  useEffect(() => {
+    // Retrieve user information from localStorage
+    const user = localStorage.getItem('user');
+    if (user) {
+      const { name } = JSON.parse(user);
+      setUserName(name);
+    }
+  }, []);
   return (
     <div className='flex justify-between main-page-header'>
         <div className='side-bar border-r-2 side-bar-wrapper'> 
         <div className=' text-center mb-8'>
-        <h3 className='text-lg   text-white font-bold ' style={{textAlign:"center"}}>Welcome Faculty</h3>
+        <h3 className='text-lg   text-white font-bold ' style={{textAlign:"center"}}>Welcome-{userName}</h3>
         </div>
         <div>
             <ul className=' text-white cursor-pointer '>
@@ -39,7 +52,7 @@ export default function Faculty() {
                 <div>
                     <img src={Logo} alt="logo" className='header-logo-admin-panel'></img>
                 </div>
-                <div><button value={"Logout"}  onClick={logout}>Logout</button> </div>
+                <button className='pt-3 pb-3 pl-10 pr-10 bg-[#1b3058] text-white rounded-md' style={{ position: 'absolute', right: '20px' }} onClick={logout}>Logout</button>
             </header>
             <div className='min-h-max flex justify-center border-t-2'>
                 {forgotPassword ? <ResetPassword/> : ""}
